0ac85356a3e6797af6ef72ab307f29557fc42c99
unknown <naohiro@naohiro-THINK.(none)>
2011-10-27 21:34:34 +0900

    test

---
+++ Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace GoogleAppsProvisioningTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "AuthN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "AuthN code";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(17, 338);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(252, 19);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(17, 152);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Create";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(88, 181);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 19);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "username";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "password";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(88, 205);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 19);
+            this.textBox4.TabIndex = 12;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(194, 227);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 46);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Update";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "familyName";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(88, 229);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 19);
+            this.textBox5.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 254);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 12);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "givenName";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(88, 254);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 19);
+            this.textBox6.TabIndex = 17;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(21, 280);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(58, 16);
+            this.checkBox1.TabIndex = 18;
+            this.checkBox1.Text = "Enable";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(113, 152);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 19;
+            this.button6.Text = "Delete";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(195, 205);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 20;
+            this.button7.Text = "Change";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(194, 280);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 21;
+            this.button8.Text = "Change";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(21, 306);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(120, 16);
+            this.checkBox2.TabIndex = 22;
+            this.checkBox2.Text = "Force Change Pwd";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(195, 306);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 23;
+            this.button9.Text = "Update";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(88, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 19);
+            this.textBox2.TabIndex = 24;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "domain";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(21, 69);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 12);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "admin";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(88, 69);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 19);
+            this.textBox7.TabIndex = 27;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(21, 94);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 12);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "password";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(88, 94);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 19);
+            this.textBox8.TabIndex = 29;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 371);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace GoogleAppsProvisioningTest
+{    
+
+    public partial class Form1 : Form
+    {
+
+        const string HEAD = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><atom:entry xmlns:atom=\"http://www.w3.org/2005/Atom\" xmlns:apps=\"http://schemas.google.com/apps/2006\"><atom:category scheme=\"http://schemas.google.com/g/2005#kind\" term=\"http://schemas.google.com/apps/2006#user\"/>";
+        const string USR_NAME = "<apps:login userName=\"$$loginuser$$\"/>";
+        const string USR_CREATE = "<apps:login userName=\"$$loginuser$$\" password=\"$$password$$\" hashFunctionName=\"SHA-1\"/>";
+        const string PWD_CHANGE = "<apps:login password=\"$$password$$\" hashFunctionName=\"SHA-1\"/>";
+        const string PWD_FORCE_CHANGE = "<apps:login changePasswordAtNextLogin=\"$$forcechange$$\"/>";
+        const string STA_CHANGE = "<apps:login suspended=\"$$suspended$$\"/>";
+        const string NAME_FULL = "<apps:name familyName=\"$$familyName$$\" givenName=\"$$givenName$$\"/>";
+        const string NAME_FAM = "<apps:name familyName=\"$$familyName$$\"/>";
+        const string NAME_GIVEN = "<apps:name givenName=\"$$givenName$$\"/>";
+        const string BOTTOM = "</atom:entry>";
+
+        string AuthNToken;
+        string DOMAIN;
+        string ADMIN_USER;
+        string ADMIN_PWD;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            button7.Enabled = false;
+            button8.Enabled = false;
+            button9.Enabled = false;
+            checkBox1.Enabled = false;
+            checkBox2.Enabled = false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Authentication
+            if ((textBox2.Text == "") || (textBox7.Text == "") || (textBox8.Text == ""))
+            {
+                MessageBox.Show("Check Parameters");
+                return;
+            }
+            DOMAIN = textBox2.Text;
+            ADMIN_USER = textBox7.Text;
+            ADMIN_PWD = textBox8.Text;
+
+            System.Collections.Specialized.NameValueCollection ps = new System.Collections.Specialized.NameValueCollection();
+            ps.Add("accountType", "HOSTED");
+            ps.Add("Email", ADMIN_USER + "@" + DOMAIN);
+            ps.Add("Passwd", ADMIN_PWD);
+            ps.Add("service", "apps");
+            WebClient wc = new WebClient();
+            try
+            {
+                byte[] resData = wc.UploadValues(@"https://www.google.com/accounts/ClientLogin", ps);
+                wc.Dispose();
+                string[] a = System.Text.Encoding.UTF8.GetString(resData).Split('\n');
+                foreach (string str in a)
+                {
+                    if (str.StartsWith("Auth="))
+                    {
+                        label1.Text = str;
+                        AuthNToken = str.Substring(5);
+                    }
+                }
+                button4.Enabled = true;
+                button5.Enabled = true;
+                button6.Enabled = true;
+                button7.Enabled = true;
+                button8.Enabled = true;
+                button9.Enabled = true;
+                checkBox1.Enabled = true;
+                checkBox2.Enabled = true;
+                textBox1.Text = "operation completed";
+                button1.Enabled = false;
+            }
+            catch (System.Net.WebException ex)
+            {
+                //HttpWebResponseを取得
+                System.Net.HttpWebResponse errres =
+                    (System.Net.HttpWebResponse)ex.Response;
+                textBox1.Text =
+                    errres.StatusCode.ToString() + ":" + errres.StatusDescription;
+
+            }
+            finally
+            {
+                wc.Dispose();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // Update Name
+            WebClient wc = new WebClient();
+            wc.Headers.Add("Content-type", "application/atom+xml");
+            wc.Headers.Add("Authorization", "GoogleLogin auth=" + AuthNToken);
+
+            string str_user = HEAD +
+                              NAME_FULL.Replace("$$familyName$$", textBox5.Text).Replace("$$givenName$$", textBox6.Text) + 
+                              BOTTOM;
+
+            textBox1.Text = wc.UploadString("https://www.google.com/a/feeds/" + DOMAIN + "/user/2.0/" + textBox3.Text, "PUT", str_user);
+            wc.Dispose();
+
+        }
+
+        private string sha1_create(string original)
+        {
+            byte[] byteValue = Encoding.UTF8.GetBytes(original);
+
+            // SHA1のハッシュ値を取得する
+            SHA1 crypto = new SHA1CryptoServiceProvider();
+            byte[] hashValue = crypto.ComputeHash(byteValue);
+
+            // バイト配列をUTF8エンコードで文字列化
+            StringBuilder hashedText = new StringBuilder();
+            for (int i = 0; i < hashValue.Length; i++)
+            {
+                hashedText.AppendFormat("{0:X2}", hashValue[i]);
+            }
+            return hashedText.ToString();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            // Change Password
+            WebClient wc = new WebClient();
+            wc.Headers.Add("Content-type", "application/atom+xml");
+            wc.Headers.Add("Authorization", "GoogleLogin auth=" + AuthNToken);
+
+            string str_user = HEAD +
+                              PWD_CHANGE.Replace("$$password$$", sha1_create(textBox4.Text)) +
+                              BOTTOM;
+
+            textBox1.Text = wc.UploadString("https://www.google.com/a/feeds/" + DOMAIN + "/user/2.0/" + textBox3.Text, "PUT", str_user);
+            wc.Dispose();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            // Change Status
+            WebClient wc = new WebClient();
+            wc.Headers.Add("Content-type", "application/atom+xml");
+            wc.Headers.Add("Authorization", "GoogleLogin auth=" + AuthNToken);
+
+            string status;
+            if (checkBox1.Checked == false)
+            {
+                status = "true";
+            }
+            else
+            {
+                status = "false";
+            }
+
+            string str_user = HEAD +
+                              STA_CHANGE.Replace("$$suspended$$", status) +
+                              BOTTOM;
+
+            textBox1.Text = wc.UploadString("https://www.google.com/a/feeds/" + DOMAIN + "/user/2.0/" + textBox3.Text, "PUT", str_user);
+            wc.Dispose();
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            // Force user to change password on next login
+            WebClient wc = new WebClient();
+            wc.Headers.Add("Content-type", "application/atom+xml");
+            wc.Headers.Add("Authorization", "GoogleLogin auth=" + AuthNToken);
+
+            string force;
+            if (checkBox2.Checked == true)
+            {
+                force = "true";
+            }
+            else
+            {
+                force = "false";
+            }
+
+            string str_user = HEAD +
+                              PWD_FORCE_CHANGE.Replace("$$forcechange$$", force) +
+                              BOTTOM;
+
+            textBox1.Text = wc.UploadString("https://www.google.com/a/feeds/" + DOMAIN + "/user/2.0/" + textBox3.Text, "PUT", str_user);
+            wc.Dispose();
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // Delete user
+            WebClient wc = new WebClient();
+            try
+            {
+                wc.Headers.Add("Content-type", "application/atom+xml");
+                wc.Headers.Add("Authorization", "GoogleLogin auth=" + AuthNToken);
+
+                wc.UploadString("https://www.google.com/a/feeds/" + DOMAIN + "/user/2.0/" + textBox3.Text, "DELETE", "");
+                textBox1.Text = "operation completed";
+            }
+            catch (System.Net.WebException ex)
+            {
+                //HttpWebResponseを取得
+                System.Net.HttpWebResponse errres =
+                    (System.Net.HttpWebResponse)ex.Response;
+                textBox1.Text =
+                    errres.StatusCode.ToString() + ":" + errres.StatusDescription;
+ 
+            }
+            finally
+            {
+                wc.Dispose();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Create User
+            WebClient wc = new WebClient();
+            try
+            {
+                wc.Headers.Add("Content-type", "application/atom+xml");
+                wc.Headers.Add("Authorization", "GoogleLogin auth=" + AuthNToken);
+
+                string str_user = HEAD +
+                                  USR_CREATE.Replace("$$loginuser$$", textBox3.Text).Replace("$$password$$", sha1_create(textBox4.Text)) +
+                                  NAME_FULL.Replace("$$familyName$$", textBox5.Text).Replace("$$givenName$$", textBox6.Text) +
+                                  BOTTOM;
+                wc.Encoding = System.Text.Encoding.UTF8;
+                textBox1.Text = wc.UploadString("https://www.google.com/a/feeds/" + DOMAIN + "/user/2.0", str_user);
+            }
+            catch (System.Net.WebException ex)
+            {
+                //HttpWebResponseを取得
+                System.Net.HttpWebResponse errres =
+                    (System.Net.HttpWebResponse)ex.Response;
+                textBox1.Text =
+                    errres.StatusCode.ToString() + ":" + errres.StatusDescription;
+
+            }
+            finally
+            {
+                wc.Dispose();
+            }
+        }
+
+    }
+}
